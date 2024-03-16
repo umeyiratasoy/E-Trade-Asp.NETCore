b5b7085cfe74f67fe70b70baf9a9447066dfb78a
umeyiratasoy <umeyiratasoyy@gmail.com>
2024-03-16 16:04:42 +0300

    SEO optimization of filename

--- Infrastructure/ETicaretAPI.Infrastructure/obj/Debug/net6.0/ETicaretAPI.Infrastructure.AssemblyInfo.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/obj/Debug/net6.0/ETicaretAPI.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6665f8453bb5a0dcaf830dd978e233e60c3f8b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428279fbe0ddcf47c516acd1bce731fef8a8e3c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Presentation/ETicaretAPI.API/obj/Debug/net6.0/ETicaretAPI.API.AssemblyInfo.cs
+++ Presentation/ETicaretAPI.API/obj/Debug/net6.0/ETicaretAPI.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETicaretAPI.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a0afaa2def7c5440ba5a795522ccaf930b2e0e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428279fbe0ddcf47c516acd1bce731fef8a8e3c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETicaretAPI.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETicaretAPI.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
